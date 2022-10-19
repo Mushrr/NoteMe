66e409df46e0ec4c82eab06e0f88277d23e45c68
Mushrr <mushroom@mails.ccnu.edu.cn>
2022-10-19 10:38:47 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import notemeMarkdownParser from './src/editor/vite-plugin-noteme-mdparser';
+// import notemeMarkdownParser from './src/editor/vite-plugin-noteme-mdparser';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    notemeMarkdownParser()
+    // notemeMarkdownParser()
   ],
   base: "/"
 })
